105d9f63852b5f46b885c5d514fbeaf94d26af5f
Kayotc <kayosistem@gmail.com>
2025-05-25 17:00:03 -0300

    adição dos produtos

--- my-app/src/reused/NavBar.jsx
+++ my-app/src/reused/NavBar.jsx
@@ -2,8 +2,8 @@ import '../assets/navbar.css'
 
 function NavBar(){
     return(
-        <div className="h-16 nav-bar">
-            
+        <div className="h-20 nav-bar">
+            <a className='link' href= "/home"> <img className='h-14 w-14 rounded-lg bg-white p-1  logo' src="../public/cruz.svg" alt="imagem da cruz" /></a>
         </div>
     )
 }
--- my-app/src/reused/Products.jsx
+++ my-app/src/reused/Products.jsx
@@ -2,10 +2,77 @@ import '../assets/products.css'
 
 function Products(){
     return (
-        <div className="rounded-lg catalog">
-            <div className="h-20 w-20 rounded-md card">
+        <div className="p-3 catalog">
 
+            <div className="wall one">
+                <div className="p-2 rounded-md card">
+                    <img src="https://dv2vs9hut5ih2.cloudfront.net/Custom/Content/Products/15/96/1596_dorflex-36cpr-p1117584_m1_637630889384169868.webp" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyuO9HMtWe-37KxhDc-squ4FHNmy4In_zc5g&s" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://farma22.vtexassets.com/arquivos/ids/184800/compre-dipirona-1g.png?v=638144900800430000" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://d1jgmae0hcnr1i.cloudfront.net/Custom/Content/Products/98/82/98821_esomeprazol-magnesio-40-mg-c-28cpr-rev-p77136_m1_638291769055311783.webp" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
             </div>
+
+            <div className="wall two">
+                <div className="p-2 rounded-md card">
+                    <img src="https://d1jgmae0hcnr1i.cloudfront.net/Custom/Content/Products/89/66/89667_tadalafila-20mg-c-4cpr-p136704_m1_638282289240798409.webp" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFYxFyEeH0aZfNa2YHkh4xukwnkv28Wl8Rc6O0CCBorGIptp5OaiQUSs5Ell6Zx8rfwoo&usqp=CAU" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://drogariavenancio.vtexassets.com/arquivos/ids/1028985/54023_z.jpg?v=638128520045900000" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://drogariaspacheco.vteximg.com.br/arquivos/ids/1406907-1000-1000/4391---preservativo-jontex-sensitive-com-3-unidades-1.jpg.jpg?v=638815472590730000" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+            </div>
+            
+            <div className="wall three">
+                <div className="p-2 rounded-md card">
+                    <img src="https://io.convertiez.com.br/m/morifarma/shop/products/images/277284/large/desodorante-aerossol-dove-original-150ml_7447.jpg" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://drogariasp.vtexassets.com/arquivos/ids/1179950/29041---sabonete-liquido-johnson%E2%80%99s-baby-banho-glicerinado-da-cabeca-aos-pes-200ml-1.jpg?v=638671897387800000" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://drogariavenancio.vtexassets.com/arquivos/ids/1100835/70901_z.jpg?v=638398252278570000" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+                <div className="p-2 rounded-md card">
+                    <img src="https://images.tcdn.com.br/img/img_prod/763758/shampoo_dove_nutricao_fusao_de_oleos_400_ml_16538_1_9b7895ccdbc3bbbff86bd4db97d9e05f.jpg" alt="Imagem do produto" />
+                    <button className='h-8 w-8'><img src="" alt="" /></button>
+                    <h2>R$ 23,49</h2>
+                </div>
+            </div>
+
         </div>
     )
 }
